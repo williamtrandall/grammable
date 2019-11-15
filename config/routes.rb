56d1597d7aa3b450ab7b549045d551ca4c3b6304
William Randall <williamtrandall@gmail.com>
2019-11-15 20:53:47 +0000

    added tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create]
+  resources :grams, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
